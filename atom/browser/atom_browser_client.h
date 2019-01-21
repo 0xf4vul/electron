2a722d5f67443098cfe024e34e5371f00d239236
deepak1556 <hop2deep@gmail.com>
2019-01-22 00:02:34 +0530

    Move GetUserAgent from ContentClient to ContentBrowserClient
    https://chromium-review.googlesource.com/c/chromium/src/+/1352086

--- atom/browser/atom_browser_client.h
+++ atom/browser/atom_browser_client.h
@@ -27,6 +27,9 @@ class SSLCertRequestInfo;
 
 namespace atom {
 
+// Returns the user agent of Electron.
+std::string GetUserAgent();
+
 class AtomResourceDispatcherHostDelegate;
 class NotificationPresenter;
 class PlatformNotificationService;
@@ -154,6 +157,8 @@ class AtomBrowserClient : public content::ContentBrowserClient,
   void OnNetworkServiceCreated(
       network::mojom::NetworkService* network_service) override;
   bool ShouldBypassCORB(int render_process_id) const override;
+  std::string GetProduct() const override;
+  std::string GetUserAgent() const override;
 
   // content::RenderProcessHostObserver:
   void RenderProcessHostDestroyed(content::RenderProcessHost* host) override;
